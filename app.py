f878af2667c9e68dcd5d796da7435beb02ff1766
Darsh Shah <darshshah1008@gmail.com>
2024-02-10 05:37:43 -0500

    Update file structure

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from model import GPT2PPL
+
+app = Flask(__name__)
+
+# Initialize the GPT2PPL model
+gpt2_model = GPT2PPL()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        results, output = gpt2_model(prompt)
+        return render_template('index.html', prompt=prompt, output=output, results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
